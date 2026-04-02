6e334f1c563a29db5cda4443a419eb98f247f1c0
Sabor Acaiteria <saboracaiteria@gmail.com>
2026-04-02 17:37:04 -0300

    fix: HUD com botões nativos blindados e Tela Cheia real - 100% de visibilidade

--- scripts/ui/touch_button.gd
+++ scripts/ui/touch_button.gd
@@ -1,16 +1,24 @@
-extends TextureButton
+extends BaseButton
 
 @export var action_name: String = ""
 
 func _ready():
-	# TextureButtons work with touch out of the box in Godot 4
-	button_down.connect(_on_button_down)
-	button_up.connect(_on_button_up)
+	# Conexão nativa para o toque/clique 🥊
+	button_down.connect(_on_down)
+	button_up.connect(_on_up)
 
-func _on_button_down():
+func _on_down():
+	if action_name == "fullscreen":
+		# Alterna o modo de tela no sistema nativo 🏁📺
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		return
+
 	if action_name != "":
 		Input.action_press(action_name)
 
-func _on_button_up():
-	if action_name != "":
+func _on_up():
+	if action_name != "" and action_name != "fullscreen":
 		Input.action_release(action_name)
