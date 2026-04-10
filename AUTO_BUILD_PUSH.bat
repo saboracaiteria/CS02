fd3d5d269f1ce8aadd460836aea742e8aade4cdf
Sabor Acaiteria <saboracaiteria@gmail.com>
2026-04-10 17:42:57 -0300

    fix: V2380 - Set weapon damage to 17 HP

--- AUTO_BUILD_PUSH.bat
+++ AUTO_BUILD_PUSH.bat
@@ -1,5 +1,5 @@
 @echo off
-set VERSION=V2370
+set VERSION=V2380
 set GODOT_PATH=C:\Users\Terminal\Documents\Godot_4.3\Godot_v4.3-stable_win64.exe
 echo [1/4] EXPORTANDO PROJETO GODOT (%VERSION%)...
 "%GODOT_PATH%" --headless --export-release "Web" "C:\Users\Terminal\Documents\CS02\index.html"
@@ -7,7 +7,7 @@ echo [2/4] APLICANDO MASTER FIX...
 node patch_html.js
 echo [4/4] SUBINDO PARA O GITHUB...
 git add .
-git commit -m "fix: %VERSION% - Fix brown screen and animations"
+git commit -m "fix: %VERSION% - Set weapon damage to 17 HP"
 echo [4/4] SUBINDO PARA O GITHUB...
 git push origin main --force
 echo FINALIZADO %VERSION% COM SUCESSO!
